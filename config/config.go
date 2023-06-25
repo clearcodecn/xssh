ffeec10b94259a0407503aac56d55bb41e414cc9
zhangmj <zhangmj1@dustess.com>
2023-06-25 16:55:13 +0800

    feat: reset config

--- config/config.go
+++ config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"github.com/mitchellh/go-homedir"
 	"github.com/syndtr/goleveldb/leveldb"
+	"log"
 	"os"
 	"path/filepath"
 )
@@ -16,8 +17,11 @@ func init() {
 	dir, _ := homedir.Dir()
 	dbPath := filepath.Join(dir, confPath)
 	os.MkdirAll(dbPath, 0777)
-
-	db, _ = leveldb.OpenFile(dbPath, nil)
+	var err error
+	db, err = leveldb.OpenFile(dbPath, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func GetDB() *leveldb.DB {
